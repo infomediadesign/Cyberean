90b6eb86200bfffcbfd7e1749b74d6a9c4e8c1da
natsuki-tan <selina.andric@stud.hochschule-heidelberg.de>
2023-09-15 02:07:29 +0200

    new level select screen
    added final version of the level-select screen, and removed all the old, and therefore redundant place holder pngs.

--- src/levelselect.h
+++ src/levelselect.h
@@ -38,14 +38,14 @@ public:
     void draw();
     levelselect(SoundPlayer *soundplayer, MusicPlayer *musicPlayer);
 
-    Texture2D levelselect1 = LoadTexture("assets/screens/level_select_screen/level_select_1.png");
-    Texture2D levelselect2 = LoadTexture("assets/screens/level_select_screen/level_select_2.png");
-    Texture2D levelselect3 = LoadTexture("assets/screens/level_select_screen/level_select_3.png");
-    Texture2D levelselect4 = LoadTexture("assets/screens/level_select_screen/level_select_4.png");
-    Texture2D levelselect5 = LoadTexture("assets/screens/level_select_screen/level_select_5.png");
-    Texture2D levelselect6 = LoadTexture("assets/screens/level_select_screen/level_select_6.png");
-    Texture2D levelselect7 = LoadTexture("assets/screens/level_select_screen/level_select_7.png");
-    Texture2D levelselect8 = LoadTexture("assets/screens/level_select_screen/level_select_8.png");
+    Texture2D levelselect1 = LoadTexture("assets/screens/level_select_screen/level_1_selected.png");
+    Texture2D levelselect2 = LoadTexture("assets/screens/level_select_screen/level_1_music.png");
+    Texture2D levelselect3 = LoadTexture("assets/screens/level_select_screen/level_2_selected.png");
+    Texture2D levelselect4 = LoadTexture("assets/screens/level_select_screen/level_2_music.png");
+    Texture2D levelselect5 = LoadTexture("assets/screens/level_select_screen/level_3_selected.png");
+    Texture2D levelselect6 = LoadTexture("assets/screens/level_select_screen/level_3_music.png");
+    Texture2D levelselect7 = LoadTexture("assets/screens/level_select_screen/level_4_selected.png");
+    Texture2D levelselect8 = LoadTexture("assets/screens/level_select_screen/level_4_music.png");
 
     Texture2D note1 = LoadTexture("assets/screens/level_select_screen/notes_1.png");
     Texture2D note2 = LoadTexture("assets/screens/level_select_screen/notes_2.png");
